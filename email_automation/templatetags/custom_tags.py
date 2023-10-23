bccd39660815471e84e07181b07c0e89f4975c32
paul <paul.sajo.official@gmail.com>
2023-10-23 17:42:42 +0530

    minor updates

--- email_automation/templatetags/custom_tags.py
+++ email_automation/templatetags/custom_tags.py
@@ -23,20 +23,21 @@ def subtract(value, arg):
 
 @register.simple_tag
 def utc_to_local(utc_datetime, user_timezone, date_format="%b. %d, %Y, %I:%M %p"):
-   
-    if not isinstance(utc_datetime, str):
-        utc_datetime = str(utc_datetime)
-
-    try:
-        user_timezone = pytz.timezone(user_timezone)  
-
-    except pytz.UnknownTimeZoneError:
-        pass
-
-    utc_datetime_obj = datetime.fromisoformat(utc_datetime)
-    utc_datetime_obj = utc_datetime_obj.replace(tzinfo=pytz.UTC)
     
-    specific_datetime = utc_datetime_obj.astimezone(user_timezone)
-    # print("date: ", specific_datetime, specific_datetime.strftime(date_format), date_format)
-    return specific_datetime.strftime(date_format)
+    if utc_datetime:
+        if not isinstance(utc_datetime, str):
+            utc_datetime = str(utc_datetime)
+
+        try:
+            user_timezone = pytz.timezone(user_timezone)  
+
+        except pytz.UnknownTimeZoneError:
+            pass
+
+        utc_datetime_obj = datetime.fromisoformat(utc_datetime)
+        utc_datetime_obj = utc_datetime_obj.replace(tzinfo=pytz.UTC)
+        
+        specific_datetime = utc_datetime_obj.astimezone(user_timezone)
+        # print("date: ", specific_datetime, specific_datetime.strftime(date_format), date_format)
+        return specific_datetime.strftime(date_format)
     
